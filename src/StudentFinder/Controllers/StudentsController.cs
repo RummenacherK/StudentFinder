038b919eb298cfd2ac11effb81252ab5077d944d
Joel Gilbert <gilbert.279@gmail.com>
2017-03-14 13:42:25 -0500

    Fixed Errors in StudentCreateMethod

--- src/StudentFinder/Controllers/StudentsController.cs
+++ src/StudentFinder/Controllers/StudentsController.cs
@@ -146,7 +146,7 @@ namespace StudentFinder.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,GradeLevel,StudentSchoolId,StudentsSchool,fName,lName,IsActive")] Student student, [Bind("id)
+        public async Task<IActionResult> Create([Bind("Id,GradeLevel,StudentSchoolId,StudentsSchool,fName,lName,IsActive")] Student student/*, [Bind("id*/)
         {
             if (ModelState.IsValid)
             {
