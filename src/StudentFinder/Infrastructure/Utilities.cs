5b3e2d7740ee702ac0e736e4d3f837ef5170ab43
RummenacherK <Krummenacher@live.com>
2017-03-15 13:57:39 -0500

    UtilitiesInProgress

--- src/StudentFinder/Infrastructure/Utilities.cs
+++ src/StudentFinder/Infrastructure/Utilities.cs
@@ -1,26 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using StudentFinder.Models;
+using StudentFinder.Controllers;
+using StudentFinder.Data;
+using StudentFinder.Infrastructure;
+using StudentFinder.Services;
+using StudentFinder.ViewModels;
+using StudentFinder.Migrations;
+using System.Linq;
 
 namespace StudentFinder.Infrastructure
 {
     public class Utilities
     {
+
+        private readonly StudentFinderContext _context;
+        public Utilities(StudentFinderContext context)
+        {
+            _context = context;
+        }
+
+
        // public TimeSpan TimeOfDay { get; }
 
-     /*   GetPeriod(DateTime)
+        public int GetPeriod(DateTime today)
         {
-            Schedule schedule = new
-            foreach(var in Schedule)
-            {
-                if (DateTime.Now >= Schedule.FromValue && DateTime.Now <= Schedule.ToValue)
+            Schedule schedule = new Schedule { };
+            
+            var hh = today.Hour;
+            var mm = today.Minute;
 
-                    return Schedule.Id;
-                else
-                    return View("Index");
+            var schedule1 = _context.Schedule.Where(s => s.SchoolId == SchoolId);
+
+                           
+            where(s => s.fromH > hh ||)
+            if ( >= Schedule.FromValue && DateTime.Now <= Schedule.ToValue)
+            {
+                return Schedule.Id;
             }
-        } */
+            else
+            {
+                return default;
+            }
+           }
+        } 
     }
 }
